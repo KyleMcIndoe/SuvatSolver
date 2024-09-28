5c544c39e914b8937daefd70b3f59472744f2971
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-28 12:33:34 -0400

    Basic functionality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,17 +11,34 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.awt.Label
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var sinput by remember { mutableStateOf("") }
+    var uinput by remember { mutableStateOf("") }
+    var vinput by remember { mutableStateOf("") }
+    var ainput by remember { mutableStateOf("") }
+    var tinput by remember { mutableStateOf("") }
 
-    MaterialTheme { // hi
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    var l by remember { mutableStateOf("") }
+
+    MaterialTheme {
+        Column {
+            TextField(
+                value = sinput,
+                onValueChange = {sinput = it},
+                label = { Text(sinput)},
+                maxLines = 1
+            )
+
+            TextField(
+                value = sinput,
+                onValueChange = {sinput = it},
+                label = { Text(sinput)},
+                maxLines = 1
+            )
         }
     }
 }
---
+++ src/main/kotlin/funcs.kt
@@ -0,0 +1,6 @@
+public abstract class funcs {
+    fun vwithouts(u: Double, a: Double, t: Double): Double {
+        return u + (a * t);
+    }
+
+}
